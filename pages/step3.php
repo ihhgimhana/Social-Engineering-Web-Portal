54586dcc713109ed1bc7f24c416593d81c7d6839
ihhgimhana <ihhgimhana@gmail.com>
2021-09-30 16:23:03 +0530

    User Interface Only

---
+++ pages/step3.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Web Portal - Step 3</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="../css/style.css" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Download the Software to Continue...
+                </div>
+
+                <a href="">
+					<img src="../images/download_button.png">
+				</a>
+
+                <p>If you want to download this software as a windows executable file, Click this <a href="">Download link</a>.</p>
+
+                <a href="step4.php">
+					<input class="login_button" type="submit" name="wheel"  value="Next">
+				</a>
+
+                <p>
+                This application is only for windows operating systems. You need a windows operating system which is above than Windows Vista. This software is a programmed by python language and also used some built in python libraries and modules. But this is not a open source software, because this python application is compiled using software called py to exe converter which is free and open source cross platform freeware software. This software is a little software which  takes tiny space. This software does not consume much RAM and CPU from your computer. This software can be used to generate a random number. This software generates a random number at a particular time. <br>
+                In addition to that you do not need this application to continue to this web portal. You can simply submit your nickname as a token which you use to start this web portal key to continue in the next step.
+                </p>
+
+            </div>
+        </div>
+    </body>
+</html>
